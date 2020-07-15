170e43d9d159a3d1d2ea1853443548695a38d80c
Gobinath <slgobinath@gmail.com>
2020-07-15 17:15:10 -0400

    Release v0.3

--- gcalendar/__init__.py
+++ gcalendar/__init__.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION = "0.2"
+VERSION = "0.3"
 
 TOKEN_STORAGE_VERSION = "v1"
 
--- setup.py
+++ setup.py
@@ -29,14 +29,14 @@ def __package_files(directory):
 
 setuptools.setup(
     name="gcalendar",
-    version="0.2",
+    version="0.3",
     description="Read Google Calendar events from your terminal.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Gobinath Loganathan",
     author_email="slgobinath@gmail.com",
     url="https://github.com/slgobinath/gcalendar",
-    download_url="https://github.com/slgobinath/gcalendar/archive/v0.2.tar.gz",
+    download_url="https://github.com/slgobinath/gcalendar/archive/v0.3.tar.gz",
     packages=setuptools.find_packages(),
     package_data={},
     install_requires=requires,
